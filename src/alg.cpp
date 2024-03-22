97754532d7e914136497983723b161529a496310
asasinb11 <kulamin01@mail.ru>
2024-03-22 21:40:49 +0300

    Complete

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len - 1; i++) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 count++;
@@ -16,63 +16,62 @@ int countPairs2(int *arr, int len, int value) {
     int j = len - 1;
     int i = 0;
     int count = 0;
-    int iTemp = 0;
-    while (i < j) {
-        if (arr[i] + arr[j] == value) {
-            count++;
-            iTemp++;
-            j--;
-        } else {
-            if (iTemp != 0) {
-                i++;
-                j += iTemp;
-                iTemp = 0;
-            }
-            if (arr[i] + arr[j] > value) {
-                j--;
-            } else if (arr[i] + arr[j] < value) {
-                i++;
+    while (arr[j] > value) {
+        j--;
+    }
+    for (int m = i; m < j; m++) {
+        for (int n = j; n > m; n--) {
+            if (arr[m] + arr[n] == value) {
+                count++;
             }
         }
     }
     return count;
 }
 
+int cbinsearch(int *arr, int size, int value) {
+    int iTemp = size / 2;
+    int counter = 0;
+    bool bFlag = true;
+    if (arr[iTemp] == value) {
+        while (arr[iTemp] == value && iTemp >= 0) {
+            iTemp--;
+        }
+        iTemp++;
+        while (arr[iTemp] == value && iTemp < size) {
+            iTemp++;
+            counter++;
+        }
+        return counter;
+    }
+    while (iTemp >= 0 && iTemp < size && bFlag) {
+        if (arr[iTemp] == value) {
+            while (iTemp >= 0 && iTemp < size && arr[iTemp] == value) {
+                counter++;
+                if (arr[iTemp] < arr[size / 2]) {
+                    iTemp--;
+                } else {
+                    iTemp++;
+                }
+            }
+            bFlag = false;
+        } else if (arr[iTemp] > value && arr[iTemp - 1] < value) {
+            return 0;
+        } else if (arr[iTemp + 1] > value && arr[iTemp] < value) {
+            return 0;
+        } else if (arr[iTemp] > value) {
+            iTemp--;
+        } else {
+            iTemp++;
+        }
+    }
+    return counter;
+}
+
 int countPairs3(int *arr, int len, int value) {
     int sum = 0;
     for (int i = 0; i < len; i++) {
-        int count = 0;
-        int iTemp = value - arr[i];
-        int l = i + 1;
-        int r = len - 1;
-            while (l < r) {
-                int mid = (l + r) / 2;
-                if (r - l == 1) {
-                    if (arr[r] == iTemp) {
-                        sum++;
-                    }
-                    if (arr[l] == iTemp) {
-                        sum++;
-                    }
-                    break;
-                }
-                if (arr[mid] < iTemp) {
-                    l = mid;
-                } else if (arr[mid] > iTemp) {
-                    r = mid;
-                } else if (arr[mid] == iTemp) {
-                    while (arr[mid] == iTemp) {
-                        mid--;
-                    }
-                    mid++;
-                    while (arr[mid] == iTemp) {
-                        mid++;
-                        count++;
-                    }
-                    break;
-                }
-            }
-        sum += count;
+        sum += cbinsearch(&arr[i + 1], len - i - 1, value - arr[i]);
     }
     return sum;
 }
